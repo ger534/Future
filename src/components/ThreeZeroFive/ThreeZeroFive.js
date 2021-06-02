780e16f83c766f0072879542242d50349777b030
ger534 <gergerger2009@gmail.com>
2021-06-01 22:49:38 -0600

    connection with api

--- src/components/ThreeZeroFive/ThreeZeroFive.js
+++ src/components/ThreeZeroFive/ThreeZeroFive.js
@@ -1,16 +1,11 @@
-import * as React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { useTranslation } from 'react-i18next';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import clsx from 'clsx';
-import { flags } from '../../flags'
-
-import { ThemeProvider } from "styled-components";
-import { lightTheme, darkTheme } from "../Themes"
-import  {useDarkMode} from "../useDarkMode"
-import { ThreeZeroFiveText } from './ThreeZeroFiveText'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -30,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
     overflow: 'auto',
     flexDirection: 'column',
     //backgroundColor: "blue"
-    
+
   },
   fixedHeight: {
     height: 240,
@@ -38,39 +33,50 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function TermsAndConditions() {
+export default function ThreeZeroFive() {
 
   const classes = useStyles();
 
   //language
   const { t } = useTranslation('common');
 
-  const [theme, themeToggler] = useDarkMode();
-  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+  const [text, SetText] = useState("");
 
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
-  //console.log(TermsAndConditionsText)
+
+  useEffect(() => {
+    axios("http://localhost:3001/threezerofive", {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      //data: payload,
+    }).then(response => {
+      console.log(response)
+      SetText(response.data)
+    })
+  }, [])
 
   return (
-      <Grid container spacing={3}>
-        <Typography color="primary" component="p" variant="h3">
-          305, humano después de todo
-        </Typography>
-        <div style={{whiteSpace: "pre-wrap"}}>{ThreeZeroFiveText}</div>
-        <Paper className={fixedHeightPaper}>
-          <Grid item md={12}>
-            <Typography color="primary" component="p" variant="h3">
-              305
-            </Typography>
-            <Typography component="p" variant="subtitle1">
+    <Grid container spacing={3}>
+      <Typography color="primary" component="p" variant="h3">
+        305, humano después de todo
+      </Typography>
+      <div style={{ whiteSpace: "pre-wrap" }}>{text}</div>
+      <Paper className={fixedHeightPaper}>
+        <Grid item md={12}>
+          <Typography color="primary" component="p" variant="h3">
+            305
+          </Typography>
+          <Typography component="p" variant="subtitle1">
 
-            <div style={{whiteSpace: "pre-wrap"}}>{ThreeZeroFiveText}</div>
+            <div style={{ whiteSpace: "pre-wrap" }}>{text}</div>
 
           </Typography>
-          </Grid>
-        </Paper>
+        </Grid>
+      </Paper>
 
-      </Grid>
+    </Grid>
 
   );
 }
