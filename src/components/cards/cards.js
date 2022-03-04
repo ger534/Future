c7453277e68b882c7a5e98b70048f4fa8d3c7304
ger534 <gergerger2009@gmail.com>
2022-03-04 04:41:30 -0600

    ghost cards logic (WIP?)

--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -1,63 +1,25 @@
 import React, { useEffect, useState } from 'react';
 
+/* third party packages */
 import Grid from '@material-ui/core/Grid'
 import useMediaQuery from '@material-ui/core/useMediaQuery';
-
-import CustomCard from '../../libs/customCard/customCard';
-
-//import X035 from '../../assets/X035.png'
-//import Test from '../../assets/Test.png'
-import Test from '../../assets/Test2.png'
 import { Container } from '@material-ui/core';
+
+/* helpers */
 import useWindowDimensions from '../../helpers/useWindowDimensions/useWindowDimensions';
 
+/* intellectual property */
+import CustomCard from '../../libs/customCard/customCard';
+import Test from '../../assets/Test2.png'
 
-let secondaryCardsArray = [
-    {
-        title: "305",
-        text: "La introducción",
-        image: Test,
-    },
-    {
-        title: "99",
-        text: "La sombra",
-        image: Test,
-    },
-    {
-        title: "696",
-        text: "El enamorado",
-        image: Test,
-    },
-
-    {
-        title: "1",
-        text: "La introducción",
-        image: Test,
-    },
-    {
-        title: "2",
-        text: "La sombra",
-        image: Test,
-    },
-    {
-        title: "3",
-        text: "El enamorado",
-        image: Test,
-    },
-    {
-        title: "4",
-        text: "La introducción",
-        image: Test,
-    },
-    {
-        title: "5",
-        text: "La sombra",
-        image: Test,
-    },
-]
+//routing
+import { useHistory } from "react-router-dom";
 
 
 function Cards(props) {
+
+    const history = useHistory();
+
     const matches = useMediaQuery('(min-width:720px)');
     const { height, width } = useWindowDimensions();
 
@@ -65,41 +27,35 @@ function Cards(props) {
     const [secondaryCards, setSecondaryCards] = useState([])
 
     const fillWithGhosts = (elementsPerRow, array, setter) => {
-        if(elementsPerRow === 1) {elementsPerRow = 2}
-        if(elementsPerRow > 4) {elementsPerRow = 4}
+        if (elementsPerRow === 1) { elementsPerRow = 2 }
+        if (elementsPerRow > 4) { elementsPerRow = 4 }
         let length = array.length
         let cardGhosted = [...array]
-        while(length % elementsPerRow !== 0){
+        while (length % elementsPerRow !== 0) {
             cardGhosted.push({ title: "", image: Test })
-            length=length+1
+            length = length + 1
         }
         setter(cardGhosted)
     }
 
     useEffect(() => {
-        fillWithGhosts(Math.floor(width/350), props.mainCards, setCards)
-        fillWithGhosts(Math.floor(width/350), secondaryCardsArray, setSecondaryCards)
+        fillWithGhosts(Math.floor(width / 350), props.mainCards, setCards)
     }, [height, width, props.mainCards])
 
     return (
         <>
-            <h1 style={{ textAlign: "center" }}>Historia principal</h1>
+            <h1 style={{ textAlign: "center" }}>{`<Personajes principales/>`}</h1>
+
             <Container>
                 <Grid container justify={matches ? "space-evenly" : "center"} >
                     {cards.map((card, i) =>
-                        <Grid item key={i}>
-                            <CustomCard title={card.title} text={card.text} image={card.image} route={card.title}></CustomCard>
-                        </Grid>
-                    )}
-                </Grid>
-            </Container>
-            <h1 style={{ textAlign: "center" }}>Subtramas</h1>
-            <Container>
-                <Grid container justify={matches ? "space-evenly" : "center"}>
-                    {secondaryCards.map((card, i) =>
-                        <Grid item key={i}>
-                            <CustomCard title={card.title} text={card.text} image={card.image}></CustomCard>
-                        </Grid>
+                        <div style={{ width: "30%"/*, padding: "0px"*/ }} key={i}>
+                            {/*<CustomCard title={card.title} text={card.text} image={card.image} route={card.title}></CustomCard>*/}
+                            <div onClick={() => { history.push(card.route) }} style={{ cursor:"pointer", paddingTop: "1%", margin: "1px", display: "flex", justifyContent: "center", flexDirection: "column", border: "1px solid ", alignItems: "center" }}>
+                                <img src={card.image} width={"100%"} height={"30%"} style={{ s: "1px" }} alt={`image ${card.title}`} />
+                                <h1 style={{ textAlign: "center" }}>{card.title}</h1>
+                            </div>
+                        </div>
                     )}
                 </Grid>
             </Container>
