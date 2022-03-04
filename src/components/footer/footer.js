a1758101a988f31e7582583a818bae6e1b84a0c9
ger534 <gergerger2009@gmail.com>
2022-03-04 04:45:31 -0600

    new footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+
+
+import { Typography, useMediaQuery } from '@material-ui/core';
+
+/* helpers */
+import { ThemeContext } from '../../helpers/themeContext/themeContext';
 
 export default function Footer(props) {
 
+  const matches = useMediaQuery('(min-width:1200px)');
+
+  const [contextTheme] = React.useContext(ThemeContext);
+
   return (
     <>
-      <div style={{ padding: "10px", backgroundColor: "#011423", color: props.contextTheme.elements1, borderTop: `1px solid` }}>
+      {matches && <div style={{ padding: "10px", backgroundColor: "#011423", color: props.theme.elements1, borderTop: `1px solid` }}>
         <Typography variant="body2" /*align="center"*/ {...props}>
-          Tecnotopía. San José, Costa Rica 2021.
+
+          Tecnotopía. San José, Costa Rica 2021. <>Este trabajo se encuentra bajo la licencia.&nbsp;
+            <a style={{ color: contextTheme.details }} rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.es" rel="noopener noreferrer" target="_blank">
+              Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+            </a>. <img alt="Creative Commons License" style={{ borderWidth: 0 }} src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></>
+
+
         </Typography>
-      </div>
+
+
+      </div>}
     </>
   );
 }
