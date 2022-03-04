5890fb3a3f903890356c6a0f61e64ffe0a76d2a7
ger534 <gergerger2009@gmail.com>
2022-03-04 04:46:31 -0600

    new footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -16,9 +16,8 @@ export default function Footer(props) {
     <>
       {matches && <div style={{ padding: "10px", backgroundColor: "#011423", color: props.theme.elements1, borderTop: `1px solid` }}>
         <Typography variant="body2" /*align="center"*/ {...props}>
-
           Tecnotopía. San José, Costa Rica 2021. <>Este trabajo se encuentra bajo la licencia.&nbsp;
-            <a style={{ color: contextTheme.details }} rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.es" rel="noopener noreferrer" target="_blank">
+            <a style={{ color: contextTheme.details }} rel="license noopener noreferrer" href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.es" target="_blank">
               Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
             </a>. <img alt="Creative Commons License" style={{ borderWidth: 0 }} src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></>
 
