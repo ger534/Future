dc2b7e0a61822e35dfb8d7a0a37aba5229885d94
ger534 <gergerger2009@gmail.com>
2022-03-04 05:38:51 -0600

    app and appbar changes

--- src/libs/appbar/appbar.js
+++ src/libs/appbar/appbar.js
@@ -1,85 +1,236 @@
+import React, { useState } from 'react';
+
+/* third party packages */
 import clsx from 'clsx';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Badge from '@material-ui/core/Badge';
+import Typography from '@material-ui/core/Typography';
+import { useTranslation } from 'react-i18next';
+import { Box, Tab, Tabs, useMediaQuery } from '@material-ui/core';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import Typography from '@material-ui/core/Typography';
+import { Button } from '@material-ui/core';
 
-/**
- * icons
- */
-//import LanguageIcon from '@material-ui/icons/Language';
+/* icons */
 import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 import Brightness6Icon from '@material-ui/icons/Brightness6';
+import Person from '@material-ui/icons/Person';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+import LanguageIcon from '@material-ui/icons/Language';
 
-import tecnotopia_black_v2 from '../../assets/tecnotopia_black_v2.png'
+/* intellectual property */
+import tecnotopia from '../../assets/tecnotopia.svg'
+import { ThemeContext } from '../../helpers/themeContext/themeContext';
+import Modal from '../../libs/modal/modal';
 
-import { useTranslation } from 'react-i18next';
+/* routing */
+import { useHistory } from "react-router-dom";
+import { Link } from 'react-router-dom';
+
+
 
 export default function TecnotopiaAppBar(props) {
+
+    const contextUser = JSON.parse(sessionStorage.getItem('user'));
+
+    const history = useHistory();
+
+    const matches = useMediaQuery('(min-width:1200px)');
+
+    //WIP language dialog
+    const [openLanWIP, setOpenLanWIP] = useState(false);
 
     //language
     const { i18n } = useTranslation('common');
 
+    const [languageAnchorEl, setLanguageAnchorEl] = useState(null);
+    const handleLanguageOpen = (event) => {
+        setLanguageAnchorEl(event.currentTarget);
+    };
+    const handleLanguageClose = (lan, event) => {
+        if (typeof lan === 'string') {
+            console.log("lan", lan)
+            if(lan.toString() !== 'es'){
+                //open WIP modal here
+                setOpenLanWIP(true)
+            }else{
+                i18n.changeLanguage(lan.toString())
+            }
+        }
+        setLanguageAnchorEl(null);
+    };
+
+    const [openDrawer, setOpenDrawer] = useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpenDrawer(true);
+    };
+    const handleDrawerClose = () => {
+        setOpenDrawer(false);
+    };
+
+    const [contextTheme] = React.useContext(ThemeContext);
+
+    //main options for appbar 
+    const mainOptions = [
+        { title: <h2 onClick={() => { history.push("/") }}>Inicio</h2>, route: "/" },
+        { title: <h2 onClick={() => { history.push("/plays") }}>Obras disponibles</h2>, route: "/plays" },
+        { title: <h2>Objetivos</h2>, route: "/project_presentation" },
+        { title: <h2>Autores</h2>, route: "/authors" },
+        { title: <h2>¿Quieres publicar?</h2>, route: "/info" },
+    ]
+
+    /* CURRENT TAB NEEDS TO BE A COOKIE */
+    //useState(JSON.parse(localStorage.getItem('currentTabCookie')) ? JSON.parse(localStorage.getItem('currentTabCookie')) : 0)
+    const [currentTab, setCurrentTab] = useState(JSON.parse(localStorage.getItem('currentTabCookie')) ? JSON.parse(localStorage.getItem('currentTabCookie')) : 0)
+
+    const handleTabChange = (event, newTab) => {
+        setCurrentTab(newTab);
+        localStorage.setItem('currentTabCookie', newTab)
+    };
+
     return (
+        <>
+            <Modal
+                open={openLanWIP} setOpen={setOpenLanWIP}
+                description={<span style={{ fontSize: "30px", fontFamily: "OpenSans", fontWeight: "bold" }}>Contenido en desarrollo</span>}
+                //description={<h1>Contenido en desarrollo</h1>}
+                actions={<Button onClick={() => { setOpenLanWIP(false) }} color="primary" autoFocus>Ok</Button>} />
 
-        <AppBar position="absolute" className={clsx(props.classes.appBar, props.openDrawer && props.classes.appBarShift)}>
-            <Toolbar className={props.classes.toolbar}>
-                <IconButton
-                    edge="start"
-                    color="inherit"
-                    aria-label="open drawer"
-                    aria-controls="language-menu"
-                    onClick={props.handleDrawerOpen}
-                    className={clsx(props.classes.menuButton, props.openDrawer && props.classes.menuButtonHidden)}
-                >
-                    <MenuIcon />
-                </IconButton>
-                <Typography component="h1" variant="h6" color="inherit" noWrap className={props.classes.title}>
-                    <IconButton color="inherit" href="/">
-                        <img src={tecnotopia_black_v2} width="100px" style={{ paddingTop: "10px" }} alt="tecnotopia logo" />
-                    </IconButton>
-                </Typography>
-                <IconButton color="inherit" onClick={props.toggleTheme}>
-                    <Badge color="secondary" >
-                        <Brightness6Icon />
-                    </Badge>
-                </IconButton>
-                {/*<IconButton color="inherit" onClick={props.handleLanguageOpen}>
-                    <Badge color="secondary" >
-                        <LanguageIcon />
-                    </Badge>
-                </IconButton>*/}
+            <AppBar position="fixed" className={clsx(props.classes.appBar, props.openDrawer && props.classes.appBarShift)} >
+                <Toolbar className={props.classes.toolbar}>
 
-                <Menu
-                    id="language-menu"
-                    anchorEl={props.anchorEl}
-                    keepMounted
-                    open={Boolean(props.anchorEl)}
-                    onClose={props.handleLanguageClose}
-                >
-                    {i18n.languages.map((lan) =>
-                        <MenuItem key={lan} onClick={(event) => props.handleLanguageClose(lan, event)} >{lan}</MenuItem>
-                    )}
-                </Menu>
+                    {/* logo */}
+                    <Typography component="h1" variant="h6" color="inherit" noWrap className={props.classes.title}>
+                        <IconButton color="inherit" href="/">
+                            <img src={tecnotopia} width="235px" height="51px" style={{ paddingTop: "0px" }} alt="tecnotopia logo" />
+                        </IconButton>
+                    </Typography>
 
-            </Toolbar>
-        </AppBar>
+                    {/* main options */}
+                    {matches ? <>
+                        <Tabs value={currentTab} onChange={handleTabChange} aria-label="main options tabs">
+                            {mainOptions.map((option, i) =>
+                                <Tab key={i} value={i} onClick={() => { history.push(option.route) }} label={option.title} id={`simple-tab-${i}`} aria-controls={`simple-tabpanel-${i}`}></Tab>
+                            )}
+                        </Tabs>
+
+                        {/* authentication */}
+                        <IconButton color="inherit" onClick={() => { history.push("/login") }}>
+                            <Badge color="secondary" >
+                                {contextUser ? <Person /> : <PersonOutlineIcon />}
+                            </Badge>
+                        </IconButton>
+
+                        {/*dark-light theme*/}
+                        <IconButton color="inherit" onClick={props.toggleTheme}>
+                            <Badge color="secondary" >
+                                <Brightness6Icon />
+                            </Badge>
+                        </IconButton>
+
+                        {/* language */}
+                        <IconButton color="inherit" onClick={handleLanguageOpen}>
+                            <Badge color="secondary" >
+                                <LanguageIcon />
+                            </Badge>
+                        </IconButton>
+
+                    </> :
+                        <>
+                            {/* mobile */}
+                            <IconButton color="inherit" onClick={handleDrawerOpen}>
+                                <Badge color="secondary" >
+                                    <MenuIcon />
+                                </Badge>
+                            </IconButton>
+                            <Drawer
+                                variant="temporary"
+                                classes={{
+                                    paper: props.classes.drawerPaper,
+                                }}
+                                onClose={handleDrawerClose}
+                                open={openDrawer}
+                            >
+                                {/* logo */}
+                                <div style={{ paddingTop: "15%", display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
+                                    <img src={tecnotopia} width="235px" height="51px" style={{}} alt="tecnotopia logo" />
+
+                                    {/* authentication */}
+                                    <IconButton color="inherit" onClick={() => { history.push("/login") }}>
+                                        <Badge color="secondary" >
+                                            {contextUser ? <Person /> : <PersonOutlineIcon />}
+                                        </Badge>
+                                        Ingresar
+                                    </IconButton>
+                                </div>
+
+
+
+                                <Divider />
+                                <List onClick={handleDrawerClose} style={{ overflowY: "auto", overflowX: "hidden" }}>
+                                    {/*mainOptions*/}
+                                    {mainOptions.map((item, i) =>
+                                        <Link key={item.text} to={item.route} style={{ textDecoration: 'none', color: contextTheme.foreground }} >
+                                            <ListItem button>
+                                                <ListItemText primary={item.title} />
+                                            </ListItem>
+                                        </Link>
+                                    )}
+                                </List>
+                                <Divider />
+                                <div style={{ display: "flex" }}>
+                                    {/*dark-light theme*/}
+                                    <IconButton color="inherit" onClick={props.toggleTheme}>
+                                        <Badge color="secondary" >
+                                            <Brightness6Icon />
+                                        </Badge>
+                                    </IconButton>
+
+                                    {/* language */}
+                                    <IconButton color="inherit" onClick={handleLanguageOpen}>
+                                        <Badge color="secondary" >
+                                            <LanguageIcon />
+                                        </Badge>
+                                    </IconButton>
+                                </div>
+                                <Divider />
+                                <div style={{ padding: "10px", fontSize: "15px", whiteSpace: "pre-wrap" }}>
+                                    <span style={{}}>Este trabajo se encuentra bajo la licencia &nbsp;
+                                        <a style={contextTheme.foreground === "#000000" ? {} : { color: contextTheme.details }} rel="license noopener noreferrer" href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.es" target="_blank">
+                                            Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+                                        </a>.    </span>
+                                    <br />
+                                    <Box textAlign={'right'}>
+                                        <img alt="Creative Commons License" style={{ borderWidth: 0 }} src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" />
+                                    </Box>
+                                </div>
+                            </Drawer>
+                        </>
+                    }
+
+                    {/* language */}
+                    <Menu
+                        id="language-menu"
+                        anchorEl={languageAnchorEl}
+                        keepMounted
+                        open={Boolean(languageAnchorEl)}
+                        onClose={handleLanguageClose}
+                    >
+                        {i18n.languages.map((lan) =>
+                            <MenuItem key={lan} onClick={(event) => handleLanguageClose(lan, event)} >{lan}</MenuItem>
+                        )}
+                    </Menu>
+
+                </Toolbar>
+            </AppBar>
+        </>
     );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/main/App.js
+++ src/main/App.js
@@ -4,28 +4,39 @@ import Container from '@material-ui/core/Container';
 
 /* third party packages */
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
 
 /* intellectual property */
 import Home from '../components/home/Home'
 import Chapter from '../components/chapter/chapter'
+import Tale from '../components/tale/tale'
 import Cards from '../components/cards/cards'
 import Plots from '../components/plots/plots'
 import Characters from '../components/characters/characters'
 import { ThemeContext, themes } from '../helpers/themeContext/themeContext';
 import Footer from '../components/footer/footer';
 import Game from "../components/game/game";
+import Login from "../components/login/login";
+import SignUp from "../components/signUp/signUp";
+import { UserContext } from "../helpers/userContext/userContext";
+import ProjectPresentation from "../components/projectPresentation/projectPresentation";
+import About from "../components/about/About";
+import Info from "../components/info/Info";
+import NotReady from "../components/notReady/notReady";
 
-//character cards array
-import { mainCards, chapters, games } from './data'
+
+import GameCover from "../components/gameCover/gameCover";
+
+//data
+import { gameCovers, mainCards, mainPlays, chapters, tales, games } from './data'
 import useStyles from './style';
 
 import TecnotopiaAppBar from '../libs/appbar/appbar';
-import TecnotopiaDrawer from '../libs/drawer/drawer';
+import Plays from "../components/plays/plays";
+import useWindowDimensions from "../helpers/useWindowDimensions/useWindowDimensions";
+import { Tab } from "@material-ui/core";
 
 function App() {
-  //language
-  const { i18n } = useTranslation('common');
+  
   const [openDrawer, setOpenDrawer] = useState(false);
   const handleDrawerOpen = () => {
     setOpenDrawer(true);
@@ -33,85 +44,90 @@ function App() {
   const handleDrawerClose = () => {
     setOpenDrawer(false);
   };
-  const [anchorEl, setAnchorEl] = useState(null);
-  const handleLanguageOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleLanguageClose = (lan, event) => {
-    if (typeof lan === 'string') {
-      i18n.changeLanguage(lan.toString())
-    }
-    setAnchorEl(null);
-  };
-  const [contextTheme, themeToggler] = useState(JSON.parse(localStorage.getItem('contextTheme')) ? JSON.parse(localStorage.getItem('contextTheme')) : themes.light);
-  const classes = useStyles(contextTheme);
+  
+  const [contextTheme, themeToggler] = useState(JSON.parse(localStorage.getItem('contextTheme')) ? JSON.parse(localStorage.getItem('contextTheme')) : themes.dark);
   const toggleTheme = () => {
     localStorage.setItem('contextTheme', JSON.stringify(themes.dark) === localStorage.getItem('contextTheme') ? JSON.stringify(themes.light) : JSON.stringify(themes.dark))
     themeToggler(JSON.parse(localStorage.getItem('contextTheme')))
   };
+  const classes = useStyles(contextTheme);
+
+  const { height } = useWindowDimensions();
+
+  const [contextUser, setContextUser] = useState(JSON.parse(sessionStorage.getItem('user')) ? JSON.parse(sessionStorage.getItem('user')) : {});
+  const setUser = (user) => {
+    localStorage.setItem('user', JSON.stringify(user))
+    setContextUser(JSON.parse(localStorage.getItem('user')))
+  };
 
   return (
     <>
-      <ThemeContext.Provider value={[contextTheme, themeToggler]}>
-        <div className={classes.root}>
-          <CssBaseline />
-          <Router>
+      <br></br>
+      <UserContext.Provider value={[contextUser, setContextUser]}>
+        <ThemeContext.Provider value={[contextTheme, themeToggler]}>
+          <div className={classes.root} style={{ minHeight: `${height - 41}px` }}>
+            <CssBaseline />
+            <Router>
 
-            <TecnotopiaAppBar
-              toggleTheme={toggleTheme}
-              classes={classes}
-              anchorEl={anchorEl}
-              openDrawer={openDrawer}
-              handleDrawerOpen={handleDrawerOpen}
-              handleLanguageOpen={handleLanguageOpen}
-              handleLanguageClose={handleLanguageClose} />
+              <TecnotopiaAppBar
+                toggleTheme={toggleTheme}
+                classes={classes}
+                openDrawer={openDrawer}
+                handleDrawerOpen={handleDrawerOpen} />
 
-            <TecnotopiaDrawer
-              chapters={chapters}
-              games={games}
-              toggleTheme={toggleTheme}
-              classes={classes}
-              openDrawer={openDrawer}
-              handleDrawerClose={handleDrawerClose} />
+              <main className={classes.content}>
+                <div className={classes.appBarSpacer} />
+                
+                  <Switch onClick={handleDrawerClose}>
 
-            <main className={classes.content}>
-              <div className={classes.appBarSpacer} />
-              <Container /*maxWidth="lg"*/ maxWidth={false} className={classes.container}>
+                    <Route path="/plays" component={() => <Plays mainPlays={mainPlays} />} />
+                    <Route path="/characters" component={() => <Cards mainCards={mainCards} />} />
+                    <Route path="/plots" component={Plots} />
+                    <Route path="/project_presentation" component={ProjectPresentation} />
+                    <Route path="/authors" component={About} />
+                    <Route path="/info" component={Info} />
+                    <Route path="/X035" component={NotReady} />
 
-                <Switch onClick={handleDrawerClose}>
-                  {/*<Route path="/dashboard" component={Dashbord} />*/}
-                  {/*<Route path="/about" component={About} />*/}
-                  {/*<Route path="/termsandconditions" component={TermsAndConditions} />*/}
+                    {/* auth paths */}
+                    <Route path="/login" component={Login} setUser={setUser} />
+                    <Route path="/signup" component={SignUp} setUser={setUser} />
 
-                  <Route path="/cards" component={() => <Cards mainCards={mainCards} />} onClose={handleDrawerClose} onClick={handleDrawerClose} />
-                  <Route path="/plots" component={Plots} onClose={handleDrawerClose} onClick={handleDrawerClose} />
+                    {/* cover paths */}
+                    {gameCovers.map((gameCover, i) =>
+                      <Route key={i} path={gameCover.route} render={() => <GameCover title={gameCover.title} image={gameCover.image} acts={gameCover.acts} extraContent={gameCover.extraContent} />} onClose={handleDrawerClose} onClick={handleDrawerClose} />
+                    )}
 
-                  {/*interactive game paths*/}
-                  {games.map((game) =>
-                    <Route key={game.gameId} path={game.route} render={() => <Game gameId={game.gameId} route={game.route} next={game.next} />} onClose={handleDrawerClose} onClick={handleDrawerClose} />
-                  )}
+                    {/* interactive game paths */}
+                    {games.map((game) =>
+                      <Route key={game.gameId} path={game.route} render={() => <Game gameId={game.gameId} image={game.image} route={game.route} next={game.next} />} onClose={handleDrawerClose} onClick={handleDrawerClose} />
+                    )}
 
-                  {/*characters paths*/}
-                  {mainCards.map((character) =>
-                    <Route path={`/${character.title}`} key={character.title} render={() => <Characters char={character.title} image={character.image} />} onClose={handleDrawerClose} onClick={handleDrawerClose} />
-                  )}
-                  
-                  {/*static stories paths*/}
-                  {chapters.map((chapter) =>
-                    <Route key={chapter.title} path={chapter.route} render={() => <Chapter title={chapter.title} file_name={chapter.file_name} route={chapter.route} next={chapter.next} />} onClose={handleDrawerClose} onClick={handleDrawerClose} />
-                  )}
+                    {/* characters paths */}
+                    {mainCards.map((character) =>
+                      <Route path={`/${character.title}`} key={character.title} render={() => <Characters char={character.title} image={character.image} />} onClose={handleDrawerClose} onClick={handleDrawerClose} />
+                    )}
 
-                  <Route render={() => <Home />} />
-                </Switch>
+                    {/* solo tales */}
+                    {tales.map((tale) =>
+                      <Route key={tale.title} path={tale.route} render={() => <Tale title={tale.title} file_name={tale.file_name} route={tale.route} cover={tale.cover} />} />
+                    )}
 
-                <Footer style={{ /*minHeight: "100vh"*/ marginTop: "auto" }} />
+                    {/* static stories paths */}
+                    {/* drafts */}
+                    {chapters.map((chapter) =>
+                      <Route key={chapter.title} path={chapter.route} render={() => <Chapter title={chapter.title} file_name={chapter.file_name} route={chapter.route} next={chapter.next} />} onClose={handleDrawerClose} onClick={handleDrawerClose} />
+                    )}
 
-              </Container>
-            </main>
+                    <Route render={() => <Home />} />
+                  </Switch>
 
-          </Router>
-        </div>
-      </ThemeContext.Provider>
+              </main>
+
+            </Router>
+          </div>
+          <Footer theme={contextTheme} />
+        </ThemeContext.Provider>
+      </UserContext.Provider>
     </>
   );
 }
